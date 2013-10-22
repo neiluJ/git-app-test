17e98e753ba7757cfd5179b5b6f86149b441b808
neiluj <neiluj@baltringue>
2013-10-22 18:29:19 +0200

    powah!

--- app/TestGit/pages/blob/display-text.php
+++ app/TestGit/pages/blob/display-text.php
@@ -1,1 +1,17 @@
-<div class="code-display"><?php echo htmlentities($this->blob->getContent(), ENT_QUOTES, "utf-8"); ?></div>
+<table class="blob">
+    <tbody>
+        <tr>
+            <td class="ln">
+                <?php
+                $totalLines = count(explode("\n", $this->blob->getContent()));
+                for ($i = 1; $i <= $totalLines; $i++) {
+                    echo '<span>'. $i .'</span>';
+                }
+                ?>
+            </td>
+            <td class="contents code-display"><pre><code><?php echo htmlentities($this->blob->getContent(), ENT_QUOTES, "utf-8"); ?></code></pre></td>
+        </tr>
+    </tbody>
+</table>
+
+<script src="<?php echo str_replace('/index.php', '', $vh->url()); ?>/js/highlight.js/highlight.pack.js"></script>
