a348983f05214833f7aa8691f0e3d917df888161
forgery <forgery@nitronet.org>
2014-07-10 16:47:00 +0200

    better favicon

--- app/TestGit/pages/_header.php
+++ app/TestGit/pages/_header.php
@@ -9,11 +9,21 @@
 
     <title><?php if(isset($page_title)): ?><?php echo $vh->escape($page_title); ?> &bullet; <?php endif; ?><?php echo $vh->escape($vh->appTitle()); ?></title>
 
-    <link rel="icon" type="image/x-icon" href="<?php echo str_replace('/index.php', '/', $vh->url()); ?>favicon.ico" />
-    <link rel="apple-touch-icon" sizes="57x57" href="<?php echo str_replace('/index.php', '/', $vh->url()); ?>factory.png" />
-    <link rel="apple-touch-icon" sizes="114x114" href="<?php echo str_replace('/index.php', '/', $vh->url()); ?>factory.png" />
-    <link rel="apple-touch-icon" sizes="72x72" href="<?php echo str_replace('/index.php', '/', $vh->url()); ?>factory.png" />
-    <link rel="apple-touch-icon" sizes="144x144" href="<?php echo str_replace('/index.php', '/', $vh->url()); ?>factory.png" />
+      <link rel="apple-touch-icon" sizes="57x57" href="<?php echo str_replace('/index.php', '/', $vh->url()); ?>apple-touch-icon-57x57.png">
+      <link rel="apple-touch-icon" sizes="114x114" href="<?php echo str_replace('/index.php', '/', $vh->url()); ?>apple-touch-icon-114x114.png">
+      <link rel="apple-touch-icon" sizes="72x72" href="<?php echo str_replace('/index.php', '/', $vh->url()); ?>apple-touch-icon-72x72.png">
+      <link rel="apple-touch-icon" sizes="144x144" href="<?php echo str_replace('/index.php', '/', $vh->url()); ?>apple-touch-icon-144x144.png">
+      <link rel="apple-touch-icon" sizes="60x60" href="<?php echo str_replace('/index.php', '/', $vh->url()); ?>apple-touch-icon-60x60.png">
+      <link rel="apple-touch-icon" sizes="120x120" href="<?php echo str_replace('/index.php', '/', $vh->url()); ?>apple-touch-icon-120x120.png">
+      <link rel="apple-touch-icon" sizes="76x76" href="<?php echo str_replace('/index.php', '/', $vh->url()); ?>apple-touch-icon-76x76.png">
+      <link rel="apple-touch-icon" sizes="152x152" href="<?php echo str_replace('/index.php', '/', $vh->url()); ?>apple-touch-icon-152x152.png">
+      <link rel="icon" type="image/png" href="<?php echo str_replace('/index.php', '/', $vh->url()); ?>favicon-196x196.png" sizes="196x196">
+      <link rel="icon" type="image/png" href="<?php echo str_replace('/index.php', '/', $vh->url()); ?>favicon-160x160.png" sizes="160x160">
+      <link rel="icon" type="image/png" href="<?php echo str_replace('/index.php', '/', $vh->url()); ?>favicon-96x96.png" sizes="96x96">
+      <link rel="icon" type="image/png" href="<?php echo str_replace('/index.php', '/', $vh->url()); ?>favicon-16x16.png" sizes="16x16">
+      <link rel="icon" type="image/png" href="<?php echo str_replace('/index.php', '/', $vh->url()); ?>favicon-32x32.png" sizes="32x32">
+      <meta name="msapplication-TileColor" content="#00aba9">
+      <meta name="msapplication-TileImage" content="<?php echo str_replace('/index.php', '/', $vh->url()); ?>mstile-144x144.png">
 
     <!-- Bootstrap core CSS -->
     <link href="http://fonts.googleapis.com/css?family=Roboto:400,900,700,300" rel="stylesheet" type="text/css">
