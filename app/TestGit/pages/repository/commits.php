b095eacd8cc5ecc783dfd73a75649e83e8ee57a8
neiluj <julien@nitronet.org>
2015-01-15 11:23:03 +0100

    fixed commits graph

--- app/TestGit/pages/repository/commits.php
+++ app/TestGit/pages/repository/commits.php
@@ -44,7 +44,20 @@
                         }
                         ?>
                         <ul class="commits-history">
-                            <?php foreach ($finalCommits as $day => $commits): ?>
+                            <?php
+                                $lastDayInMonth = cal_days_in_month(CAL_GREGORIAN, $this->month, $this->year);
+                                $stats = array();
+                                for ($x = 1; $x <= $lastDayInMonth; $x++) {
+                                    $key = $this->year . str_pad($this->month, 2, "0", STR_PAD_LEFT) . str_pad($x, 2, "0", STR_PAD_LEFT);
+                                    if (isset($finalCommits[$key])) {
+                                        $stats[$x] = count($finalCommits[$key]);
+                                        continue;
+                                    }
+                                    $stats[$x] = 0;
+                                }
+                            ?>
+                            <?php
+                            foreach ($finalCommits as $day => $commits): ?>
                                 <li class="date">
                                     <p><b class="octicon octicon-calendar"></b> <?php $commit = $commits[0]; $date = new DateTime($commit->getCommitterDate()); echo $date->format('l d'); ?></p>
                                     <table>
@@ -145,55 +158,40 @@
 <script type="text/javascript">
     $(document).ready(function() {
         var ctx = document.getElementById("commitsContrib").getContext("2d");
-        var myLineChart = new Chart(ctx).Bar({
-                labels: [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31],
-                datasets: [
-                    {
-                        label: "My First dataset",
-                        fillColor: "rgba(220,220,220,0.5)",
-                        strokeColor: "rgba(220,220,220,0.5)",
-                        highlightFill: "rgba(220,220,220,0.75)",
-                        highlightStroke: "rgba(220,220,220,1)",
-                        data: [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31]
-                    }
-                ]
+        new Chart(ctx).Bar({
+            labels: [<?php echo implode(',', array_keys($stats)); ?>],
+            datasets: [
+                {
+                    label: "Commits",
+                    fillColor: "rgba(220,220,220,1)",
+                    strokeColor: "rgba(220,220,220,0.5)",
+                    highlightFill: "rgba(220,220,220,0.75)",
+                    highlightStroke: "rgba(220,220,220,1)",
+                    data: [<?php echo implode(',', array_values($stats)); ?>]
+                }
+            ]
         },{
-                //Boolean - Whether the scale should start at zero, or an order of magnitude down from the lowest value
-                scaleBeginAtZero : true,
-
+            //Boolean - Whether the scale should start at zero, or an order of magnitude down from the lowest value
+            scaleBeginAtZero : true,
                 //Boolean - Whether grid lines are shown across the chart
                 scaleShowGridLines : false,
-
                 //String - Colour of the grid lines
                 scaleGridLineColor : "rgba(0,0,0,.05)",
-
                 //Number - Width of the grid lines
                 scaleGridLineWidth : 1,
-
                 //Boolean - Whether to show horizontal lines (except X axis)
                 scaleShowHorizontalLines: true,
-
                 //Boolean - Whether to show vertical lines (except Y axis)
                 scaleShowVerticalLines: false,
-
                 //Boolean - If there is a stroke on each bar
                 barShowStroke : false,
-
-                //Number - Pixel width of the bar stroke
-                barStrokeWidth : 0,
-
                 //Number - Spacing between each of the X value sets
                 barValueSpacing : 0,
-
                 //Number - Spacing between data sets within X values
-                barDatasetSpacing : 0,
-
-                //String - A legend template
-                legendTemplate : "<ul class=\"<%=name.toLowerCase()%>-legend\"><% for (var i=0; i<datasets.length; i++){%><li><span style=\"background-color:<%=datasets[i].lineColor%>\"></span><%if(datasets[i].label){%><%=datasets[i].label%><%}%></li><%}%></ul>"
-
-            }
-        );
-    });
+                barDatasetSpacing : 0
+        }
+    );
+});
 
     function resizeCanvas() {
         var px = $('.col-md-7').width(), canvas = document.getElementById('commitsContrib');
